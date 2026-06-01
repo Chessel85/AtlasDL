209ed88a365ea323baebb0c77156729c004826ba
Chessel Goodwin <ckgoodwin85@gmail.com>
2026-06-01 22:14:53 +0100

    Distracted by aviation data

--- scripts/insert/InsertBaseLayerGroups.sql
+++ scripts/insert/InsertBaseLayerGroups.sql
@@ -4,4 +4,5 @@ INSERT INTO tbl_layerGroups  ( layerGroupName, selected, isBase )
     ( 'Political', 1, 1 ),  -- selected and is base layer group 
     ( 'Oceans and seas', 1, 1 ), --is selected and is base layer group 
     ( 'Water features', 1, 0 ), --is selected but is not a base layer group 
+    ( 'aviation', 1, 0 ), --is selected but is not a base layer group 
     ( 'Populated places', 1, 0 ); -- is selected but not a base layer group 
